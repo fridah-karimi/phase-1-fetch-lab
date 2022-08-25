2b7edf95e661e6176c5adb1216d74af4806f1ae8
fridah karimi <fridah.karimi@student.moringaschool.com>
2022-08-25 10:04:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let data = fetch('https://anapioficeandfire.com/api/books')
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
+  return data;
 }
 
 function renderBooks(books) {
